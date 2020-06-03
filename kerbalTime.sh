dca1e4f6e239ea1e989e65af011ca12f1d451fd2
SpaceCarmelo <65360295+SpaceCarmelo@users.noreply.github.com>
2020-06-03 23:12:42 +0200

    Update kerbalTime.sh

--- kerbalTime.sh
+++ kerbalTime.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ssh -R 5411:120.0.0.1:5411 gabriele@quaversoft.com
-ssh -R 5410:120.0.0.1:5410 gabriele@quaversoft.com
-java path/path/path/gitAutoUpdater path/di/git
+ssh -tt -R 5411:120.0.0.1:5411 gabriele@quaversoft.com &
+ssh -tt -R 5410:120.0.0.1:5410 gabriele@quaversoft.com &
+java GitAutoUpdater /Users/marulli/Desktop/COSE/SteamLibrary/steamapps/common/Kerbal\ Space\ Program/Ships/Script  
