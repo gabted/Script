d11e833332be69fd9d36db4d30e8b1ba1f7e0924
gabted <gabriele_tedeschi@outlook.com>
2020-06-18 00:04:54 +0200

    reorganizing dir

---
+++ GitAutoUpdater.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GitAutoUpdater {
+    static final int port = 5411;
+
+    //legge la risposta, o trova il file e lo invia, o invia errore 404
+    //per file txt, png e pdf manda anche l'entity header contentente
+    //il media type corretto
+    private static void respond(Socket client, String path){
+        try(
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        ){
+            System.out.println("ricevuta richiesta");
+
+            //request
+            String request = reader.readLine();
+            System.out.println("leggo header");
+            boolean headerEnded = false;
+            List<String> headers = new ArrayList<>();
+            while(!headerEnded){
+                String line = reader.readLine();
+                if(line.equals(""))
+                    headerEnded = true;     //CR-LF
+                else
+                    headers.add(line);
+            }
+
+            //response
+            writer.write("HTTP/1.1 200 ok \r\n");
+            writer.newLine();
+            writer.flush();
+
+            //pull
+            Runtime.getRuntime().exec("git pull", null, new File(path));
+        }
+        catch(IOException e){
+            System.err.println("errore comunicazione");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String path = args[0];
+        try(ServerSocket serverSocket = new ServerSocket()){
+            serverSocket.bind(new InetSocketAddress("127.0.0.1", port));
+            System.out.println("server started");
+            while(true){
+                System.out.println("server listening");
+                try{
+                    Socket client = serverSocket.accept();
+                    respond(client, path);
+                    client.close();
+                }
+                catch(IOException e){
+                    System.err.println("errore connessione");
+                    e.printStackTrace();
+                    break;
+                }
+            }
+        }
+        catch (IOException e){
+            System.err.println("errore creazione socket");
+            e.printStackTrace();
+        }
+    }
+}
--- kerbalTime.sh
+++ kerbalTime.sh
@@ -2,4 +2,4 @@
 
 ssh -fNR 5411:120.0.0.1:5411 gabriele@quaversoft.com
 ssh -fNR 5410:120.0.0.1:5410 gabriele@quaversoft.com
-java GitAutoUpdater /Users/marulli/Desktop/COSE/SteamLibrary/steamapps/common/Kerbal/Space/Program/Ships/Script  
+java /Users/marulli/Desktop/COSE/SteamLibrary/steamapps/common/Kerbal/Space/Program/Ships/Script/GitAutoUpdater /Users/marulli/Desktop/COSE/SteamLibrary/steamapps/common/Kerbal/Space/Program/Ships/Script  
