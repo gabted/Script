928cb3b439a144fb9a74b1311d5c75720cc5a737
Gabriele Tedeschi <gabriele_tedeschi@outlook.com>
2020-09-30 22:47:45 +0200

    corretto localhost

--- kerbalTime.sh
+++ kerbalTime.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ssh -fNR 5411:120.0.0.1:5411 gabriele@quaversoft.com
-ssh -fNR 5410:120.0.0.1:5410 gabriele@quaversoft.com
+ssh -fNR 5411:127.0.0.1:5411 gabriele@quaversoft.com
+ssh -fNR 5410:127.0.0.1:5410 gabriele@quaversoft.com
 java /Users/marulli/Desktop/COSE/SteamLibrary/steamapps/common/Kerbal/Space/Program/Ships/Script/GitAutoUpdater /Users/marulli/Desktop/COSE/SteamLibrary/steamapps/common/Kerbal/Space/Program/Ships/Script  
